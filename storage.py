6629729aca3f736da0f922c59c884184925919fc
art2004 <kremenkov.artem@mail.ru>
2025-10-22 16:46:43 +0300

    Частично прописал README и добавил исключения при чтении файла в storage.py

--- storage.py
+++ storage.py
@@ -5,12 +5,22 @@ from fintracker.models import Expense
 DATA_FILE = 'expenses.json'
 
 def load_expenses():
-    if not os.path.exists(DATA_FILE):
+    try:
+        if not os.path.exists(DATA_FILE):
+            return []
+        with open(DATA_FILE, 'r') as f:
+            data = json.load(f)
+            return [Expense(**item) for item in data]
+    except json.JSONDecodeError:
+        print("Ошибка: Поврежденный JSON-файл. Начинаем с пустых данных.")
         return []
-    with open(DATA_FILE, 'r') as f:
-        data = json.load(f)
-        return [Expense(**item) for item in data]
+    except Exception as e:
+        print(f"Ошибка чтения файла: {e}")
+        return []
 
 def save_expenses(expenses):
-    with open(DATA_FILE, 'w') as f:
-        json.dump([expense.to_dict() for expense in expenses], f, indent=2, ensure_ascii=False)
+    try:
+        with open(DATA_FILE, 'w') as f:
+            json.dump([expense.to_dict() for expense in expenses], f, indent=2, ensure_ascii=False)
+    except Exception as e:
+        print(f"Ошибка сохранения файла: {e}")
