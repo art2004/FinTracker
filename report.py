8baf1d3a2c33f203e875639898306f93542748cc
art2004 <kremenkov.artem@mail.ru>
2025-10-23 12:56:22 +0300

    Добавил отчёты по категориям

---
+++ report.py
@@ -0,0 +1,14 @@
+from fintracker.storage import load_expenses
+from fintracker.models import Category
+
+def generate_category_report():
+    expenses = load_expenses()
+    categories = {}
+    for expense in expenses:
+        if expense.category not in categories:
+            categories[expense.category] = Category(name=expense.category)
+        categories[expense.category].add_expense(expense.amount)
+    report = "Отчет по категориям:\n"
+    for category in categories.values():
+        report += f"{category.name}: {category.total:.2f} руб.\n"
+    return report
